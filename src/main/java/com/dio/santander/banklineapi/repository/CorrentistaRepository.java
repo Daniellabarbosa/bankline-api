4f9dda832187af8590841e928789c7e352797db0
Danyb <danyb-@live.com>
2022-05-04 17:16:59 -0300

    Primeiro commit

---
+++ src/main/java/com/dio/santander/banklineapi/repository/CorrentistaRepository.java
@@ -0,0 +1,11 @@
+package com.dio.santander.banklineapi.repository;
+
+import com.dio.santander.banklineapi.model.Correntista;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface CorrentistaRepository extends JpaRepository<Correntista, Integer> {
+
+}
